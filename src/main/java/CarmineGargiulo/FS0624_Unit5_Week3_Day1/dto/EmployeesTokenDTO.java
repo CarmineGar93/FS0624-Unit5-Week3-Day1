acc0a502f90f7026a0173e1583ed39b09915ca87
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-04 14:50:27 +0100

    added tokens dto

---
+++ src/main/java/CarmineGargiulo/FS0624_Unit5_Week3_Day1/dto/EmployeesTokenDTO.java
@@ -0,0 +1,4 @@
+package CarmineGargiulo.FS0624_Unit5_Week3_Day1.dto;
+
+public record EmployeesTokenDTO(String token) {
+}
---
+++ src/main/java/CarmineGargiulo/FS0624_Unit5_Week3_Day1/services/AuthService.java
@@ -0,0 +1,7 @@
+package CarmineGargiulo.FS0624_Unit5_Week3_Day1.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+}
